ac5a52a0c7d03dd6824e5850e8c976a4e350812a
Aditya Singh <ak9086939@gmail.com>
2026-08-23 18:18:57 +0530

    searchRoutes.js

--- backend/routes/searchRoutes.js
+++ backend/routes/searchRoutes.js
@@ -1,130 +1,60 @@
 const express = require("express");
-
 const router = express.Router();
+const User = require("../models/User");
+const Post = require("../models/Post");
 
-
-// Models import करें
-const User =
-    require("../models/User");
-
-const Post =
-    require("../models/Post");
-
-
-// ===============================
 // SEARCH USERS AND POSTS
-// ===============================
 
 router.get("/", async (req, res) => {
-
     try {
-
-        const searchText =
-            req.query.q;
-
-
+        const searchText = req.query.q;
         if (!searchText) {
-
             return res.status(400).json({
-
-                message:
-                    "Please enter search text"
-
+                message:"Please enter search text"
             });
-
         }
-
 
         // Search Users
         const users =
             await User.find({
-
                 $or: [
-
                     {
                         name: {
-
-                            $regex:
-                                searchText,
-
-                            $options:
-                                "i"
-
+                            $regex:searchText,
+                            $options:"i"
                         }
-
                     },
-
+                    
                     {
                         username: {
-
-                            $regex:
-                                searchText,
-
-                            $options:
-                                "i"
-
+                            $regex:searchText,
+                            $options:"i"
                         }
-
                     }
-
                 ]
-
-            }).select(
-
-                "name username profileImage"
-
-            );
-
+            }).select("name username profileImage");
 
         // Search Posts
         const posts =
             await Post.find({
-
                 content: {
-
-                    $regex:
-                        searchText,
-
-                    $options:
-                        "i"
-
+                    $regex:searchText,
+                    $options:"i"
                 }
 
-            }).populate(
-
-                "author",
-
-                "name username profileImage"
-
-            );
-
+            }).populate("author","name username profileImage");
 
         res.json({
-
             users: users,
-
             posts: posts
-
         });
-
 
     } catch (error) {
-
-        console.log(
-            "Search Error:",
-            error
-        );
-
-
+        console.log("Search Error:",error);
         res.status(500).json({
-
-            message:
-                "Server error"
-
+            message:"Server error"
         });
-
     }
-
 });
 
 
