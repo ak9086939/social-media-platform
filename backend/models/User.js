c75fe0ada69df0bcaef53e5ebf5fbb18b07c7f6c
Aditya Singh <ak9086939@gmail.com>
2026-08-23 18:07:35 +0530

    User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,169 +1,84 @@
 const mongoose = require("mongoose");
 
-
-// ==========================================
 // USER SCHEMA
-// ==========================================
 
 const userSchema = new mongoose.Schema(
-
     {
-
-        // ==========================================
         // NAME
-        // ==========================================
-
+        
         name: {
-
             type: String,
-
             required: true,
-
             trim: true
-
         },
 
-
-        // ==========================================
         // USERNAME
-        // ==========================================
 
         username: {
-
             type: String,
-
             required: true,
-
             unique: true,
-
             trim: true
-
         },
 
-
-        // ==========================================
         // EMAIL
-        // ==========================================
 
         email: {
-
             type: String,
-
             required: true,
-
             unique: true,
-
             trim: true,
-
             lowercase: true
-
         },
 
-
-        // ==========================================
         // PASSWORD
-        // ==========================================
 
         password: {
-
             type: String,
-
             required: true
-
         },
 
-
-        // ==========================================
         // BIO
-        // ==========================================
 
         bio: {
-
             type: String,
-
             default: "",
-
             trim: true
-
         },
 
-
-        // ==========================================
         // PROFILE IMAGE
-        // ==========================================
 
         profileImage: {
-
             type: String,
-
             default: ""
-
         },
 
-
-        // ==========================================
         // FOLLOWERS
-        // ==========================================
 
         followers: [
-
             {
-
-                type:
-
-                    mongoose.Schema.Types.ObjectId,
-
+                type:mongoose.Schema.Types.ObjectId,
                 ref: "User"
-
             }
-
         ],
 
-
-        // ==========================================
         // FOLLOWING
-        // ==========================================
 
         following: [
-
             {
-
-                type:
-
-                    mongoose.Schema.Types.ObjectId,
-
+                type:mongoose.Schema.Types.ObjectId,
                 ref: "User"
-
             }
-
         ]
-
     },
 
-
-    // ==========================================
     // TIMESTAMPS
-    // ==========================================
 
     {
-
         timestamps: true
-
     }
-
 );
 
-
-// ==========================================
 // EXPORT USER MODEL
-// ==========================================
 
-module.exports =
-
-    mongoose.model(
-
-        "User",
-
-        userSchema
-
-    );
+module.exports = mongoose.model("User",userSchema);
